4b0f884f7df256146aa82baa62ed46341db82508
Henry-Gabriel001 <henrygabriel0004@gmail.com>
2025-06-27 01:00:49 +0100

    Add files via upload

---
+++ play.js
@@ -0,0 +1,558 @@
+//define the questions and answers
+const questions=[
+{
+question:"The first book of the bible is called?",
+answers:['genesis','exodus','john','matthew'],
+corrrectAnswer:0                          
+},
+{
+question:"What are the first three word in the bible ?",
+answers:['Jesus is lord ','God love us','in the beginning','Jehovah is God'],
+corrrectAnswer:2                          
+},
+{
+  question:"How many Days God Creation take place ?",
+ answers:['6','5','7','2'],
+ corrrectAnswer:0                         
+ },
+ {
+question:"Who is the father of Abner?",
+ answers:['David','Saul','Ner','Jesse'],
+ corrrectAnswer:2                        
+},
+{
+ question:"Which book has told us that God so loved the World ?",
+answers:['matt','john','luke','psalm'],
+corrrectAnswer:1                        
+ } ,                                                        {
+ question:"When isaac is dying which food he requested ?",
+ answers:['goat meat','pepper soup','vernison','meat'],
+corrrectAnswer:2                         
+ },
+ {
+question:"Thou shall not steal is what commandment ?",
+answers:['9th','10th','7th','8th'],
+ corrrectAnswer:3                         
+ },                                                         {
+  question:"in what country jesus talk to a woman beside a wall ?",
+ answers:['Gadara','idumaea','judea','samaria'],
+ corrrectAnswer:3                         
+},
+{
+  question:"What colour did nahum say the soilders shield are ?",
+ answers:['Gold','silver','red','black'],
+ corrrectAnswer:2                         
+ }, 
+ {
+ question:"What word did paul say that stirred his audiance to anger ?",
+answers:['messiah','Gentile','jesus','Abraham'],
+corrrectAnswer:1                        
+},                      
+{
+ question:"Who met jesus in the country of the Gergesenes ?",
+answers:['his disciples','A multitude','possessed man','a harlot'],
+ corrrectAnswer:2                         
+ },                       
+ {
+ question:"How many criminals was crucified with Jesus ?",
+  answers:['6','3','7','2'],
+ corrrectAnswer:3                         
+ },                     
+ {
+ question:"What type of wood did Noah use to build the ark?",
+answers:['Gopher','plank','accacia','cedar'],
+corrrectAnswer:0                         
+ },
+ {
+ question:"Who wrestle an Angel?",
+ answers:['Adam','jacob','Esau','moses'],
+corrrectAnswer:1                         
+ }, 
+ {
+ question:"Who accused jesus of an association with beehebub ?",
+ answers:['levites','priests','pharisees','samaritans'],
+ corrrectAnswer:2                         
+ },
+ {
+  question:"What element form the status head in Nebuchadnezzar's dream ?",
+ answers:['Iron','Bronze','Gold','clay'],
+corrrectAnswer:2                        
+ },                                            
+ {
+  question:"What is the number of jacob's wives excluding concubines?",
+ answers:['1','4','7','2'],
+ corrrectAnswer:3                         
+ },                      
+ {
+   question:"who raised Esther?",
+  answers:['Nehemiah','Mordecai','jeremiah','Enoch'],
+corrrectAnswer:1                         
+},                      
+{
+  question:"After whom is the city of samaria named?",
+ answers:['omri','Ahab','shemer','luti'],
+corrrectAnswer:2                         
+ },                      
+ {
+ question:"in the beginning was the world and the word was with God, and the word was_______?",
+ answers:['Good','complete','holy','GOD'],
+ corrrectAnswer:3                         
+ },                      
+ {
+ question:"From where did paul write his letter to the Ephesians ?",
+answers:['temple','Athens','prison','timothy house'],
+ corrrectAnswer:2                         
+},                      
+{
+ question:"After the flood, what was the sign of convenant that GOD made with Noah and his Descendants ?",
+ answers:['Dry land','A dove','fire','Rainbow'],
+ corrrectAnswer:3                         
+ },                      
+ {
+  question:"Who came with paul and barnabas to jeruselem?",
+ answers:['timothy','peter','titus','john'],
+ corrrectAnswer:2                         
+},                      
+{
+ question:"Paul teaches that in christ through  we can approach God with what?",
+ answers:['confidence','gift','tithing','mourning'],
+ corrrectAnswer:0                         
+}, 
+{
+ question:" Who asked how can a man be born again?",
+ answers:['peter','Nicodemus','thomas','philip'],
+corrrectAnswer:1                        
+ },                      
+ {
+ question:"What was the name of Naomi husband ?",
+ answers:['Elimelech','Hebron','Ahab','Boaz'],
+corrrectAnswer:0                         
+},                      
+{
+question:"I am the____of sharon and the ____of the valley ?",
+answers:['child,infant','husband,wife','king,queen','Rose,lily'],
+ corrrectAnswer:3                         
+ },                      
+ {
+question:"What was Hezekiah's first order of business as king of judah ?",
+answers:['consecrating the temple','building idols','killing of isreal','taking a wife'],
+corrrectAnswer:0                         
+},                      
+{
+question:"what was the name of the chief workman of the terbernacle ?",
+ answers:['moses','Bezaleel','joshua','Dan'],
+ corrrectAnswer:1                         
+ },
+ {
+question:"What did jesus feed the 5000 with ?",
+ answers:['wine & bread','rice & beans','lamb & herb','bread & fish'],
+corrrectAnswer:3                         
+}, 
+{
+ question:"In what book does the parting of the red sea take place ?",
+ answers:['Exodus','Number','Genesis','john'],
+corrrectAnswer:0                         
+ },
+ {
+question:"When jesus enter the city of bethany he was hungry he saw a tree that didn't bear any fruit so he cursed it, what type of tree was it ?",
+answers:['fig','Apple','orange','olive'],
+ corrrectAnswer:0                         
+ },
+ {
+ question:"What does the authur of Ecclesiastes consider more bitter than death ?",
+answers:['A blessing & calamity ','an idolatarous woman','rebellious children','flesh & blood'],
+corrrectAnswer:1                         
+},                                                          {
+  question:"what does the preacher of Ecclesiates compare the word of the wise to ?",
+ answers:['Goads & nails','hammer & Anvils','precious jewels','flourishing garden'],
+ corrrectAnswer:0                         
+ },                                                         {
+   question:"Who denied knowing jesus ?",
+ answers:['peter','james','judas','john'],
+ corrrectAnswer:0                         
+}, 
+{
+question:"After jesus was baptised What form did the Holy spirit descend upon him in ?",
+ answers:['pigeon','lion','lamb','dove'],
+ corrrectAnswer:3                         
+},                                                          {
+ question:"What were the isrealites supposed to put arround the doors of their house to save themselves from the last plague in Egypt ?",
+answers:['Holy water','honey','lambs blood','their blood'],
+corrrectAnswer:2                         
+ },                       
+ {
+ question:"Who said thou art the christ son of the living God  ?",
+answers:['thomas','nathanael','peter','james'],
+ corrrectAnswer:2                         
+},                      
+{
+ question:"Where did jesus had a conversation with samaritan woman?",
+answers:['church','synagogue','At well','Galilee'],
+corrrectAnswer:2                        
+},                      
+{
+ question:"According to paul, which is a sign of spiritual maturity ?",
+answers:['following new teachings','blindness of heart','childlike innocence','speaking the truth in love'],
+ corrrectAnswer:3                         
+ },                      
+ {
+ question:"The memory of the just is blessed, but the name of the wicked shall ______ ?",
+ answers:['flourish','win','rot','bloom'],
+ corrrectAnswer:2                        
+ },                      
+ {
+ question:"To what disciple did jesus say blessed are those who not seen and yet have believed ?",
+ answers:['peter','philip','Andrew','thomas'],
+ corrrectAnswer:3                         
+ },                     
+ {
+question:"When Esther was taken to be prepared for the king what secret did mordecai tell her to keep ?",
+ answers:['she was a jew','she was a slave','she was a thief','she is married before'],
+ corrrectAnswer:0                         
+},                      
+{
+question:"But whosoever  shall lose his life for my sake and the gospel, the______?",
+ answers:['one will be king','one will not lose it','same shall save it','same shall die'],
+ corrrectAnswer:2                         
+},                     
+{
+ question:"In a failed healing attempt, what did Gehazi use to heal, an old woman's son ?",
+ answers:['jordan river','stick','Elisha staff','Moses'],
+ corrrectAnswer:2                         
+  },                      
+  {
+ question:"Of what material were all solomon drinking vessels made ?",
+ answers:['Bronze','Gold','diamond','silver'],
+ corrrectAnswer:1                         
+ },                      
+ {
+  question:"Mention of the number of beast 666 can be found where  ?",
+answers:['john','act','isaiah','Revelation'],
+ corrrectAnswer:3                         
+ },                      
+ {
+question:"Who was jehosheba ?",
+ answers:['sister of a king','wife of a priest','daughter of king','all'],
+corrrectAnswer:3                         
+},                      
+{
+  question:"How many priest were playing the trumpet at the dedication of the temple ?",
+ answers:['1200','120','300','100'],
+ corrrectAnswer:1                        
+ },                      
+ {
+ question:"The kingdom of God can be inherited by which of the followings ?",
+ answers:['robbers','drunkards','unrighteous','none'],
+ corrrectAnswer:3                         
+ },                      
+ {
+question:"who had strength because of his long hair ?",
+ answers:['samuel','elijah','gideon','samson'],
+ corrrectAnswer:3                         
+},                       
+{
+  question:"On his deathbed, who did jacob calls a fruitful bought ?",
+   answers:['isaac','Asher','benjamin','Joseph'],
+   corrrectAnswer:3 
+},
+{
+  question:" What appeared just before elijah was taken up into heaven?",
+   answers:['an oarless boat','A sack of Gold',' A chariot of fire',' A bronze ox'],
+   corrrectAnswer:2
+}, 
+{
+  question:" The dry bones raised to life appeared as what ?",
+   answers:['Goliath','Israel Ancestors',' Ghost','  Avast army'],
+   corrrectAnswer:3
+},
+{
+  question:" paul says we should do what to those that persecute us?",
+   answers:['Curse','Bless',' Forgive','  Preach'],
+   corrrectAnswer:1
+},
+{
+  question:" in the book of micah, God said woe unto them that devise what ?",
+   answers:['Hatred','Charity','Iniquity',' Murder'],
+   corrrectAnswer:2
+},
+{
+  question:" Where was jesus when the desciples asked him about his return and the end all thing?",
+   answers:['jerusalem','sea of Galilee',' Mount of olives',' Bethlehem'],
+   corrrectAnswer:2
+}, 
+{
+  question:" what part of the body is the light of the body ?",
+   answers:['Ear','Eye',' Mouth','Hand'],
+   corrrectAnswer:1
+}, 
+{
+  question:" Who did paul send back to thessalonica to see what was happening there?",
+   answers:['Timothy','silvanus',' silas','Gallio'],
+   corrrectAnswer:0
+},
+{
+  question:" What is the third miracle Moses show the pharaohs of Egypt?",
+   answers:['All water turn blood','A plague of frogs',' A plague of lice ',' famine'],
+   corrrectAnswer:2
+},
+{
+  question:" JESUS choose matthew as one of his disciples . what was matthew's occupation ?",
+   answers:['Fisherman','Tent maker',' Tax collector','Capenter'],
+   corrrectAnswer:2
+},                                                                 {
+  question:" In which city did david reign over all judah and israel?",
+   answers:['Bethlehem','Jerusalem',' Socoh',' Hebron'],
+   corrrectAnswer:1
+}, 
+{
+  question:" whose name means 'because' i asked him of the Lord?",
+   answers:['Eli','Samuel',' Hannah','Elkanah'],
+   corrrectAnswer:1
+},  
+{
+  question:" How many israelites did God use to win victory over Midianites?",
+   answers:['21000','100000',' 8000',' 300'],
+   corrrectAnswer:3
+},
+{
+  question:" Christ is the LORd of what?",
+   answers:['Livings','Deads',' living and the dead','None'],
+   corrrectAnswer:2
+}, 
+{
+  question:" The harvest is plentiful but_______?",
+   answers:['many will be hungry','The famers are few',' the labourers are few',' the sheaves are few'],
+   corrrectAnswer:2
+}, 
+{
+  question:" with what did Elisha cursed Gehazi?",
+   answers:['infidelity','Boils',' Leprosy','Hiv'],
+   corrrectAnswer:2
+},
+{
+  question:" Who avenges the defilling od dinah by killing Hamor, Shechena and the men ?",
+   answers:['Simeon and levi','Benjamin and joseph',' judah and dan',' jacob and Esau'],
+   corrrectAnswer:0
+},
+{
+  question:" Which river did not flow through the Garden of Eden ?",
+   answers:['Gihon','tigris',' Euphrates',' Jordan'],
+   corrrectAnswer:3
+},
+{
+  question:" Whose heart did the LORD open in thyatira?",
+   answers:['Lydia','Mary','Rachel',' sarah'],
+   corrrectAnswer:0
+}, 
+{
+  question:" Who did solomon petition for supplies and workers for the temple's construction ?",
+   answers:['King of Trye','King of syria',' King of Hittite','King of judah'],
+   corrrectAnswer:0
+},
+{
+  question:"Faith without ______ is dead ?",
+   answers:['water','food','work','time'],
+   corrrectAnswer:2
+},
+{
+  question:"which runaway slave became pauls son while paul was in prison  ?",
+   answers:['philemon','Timothy',' Barnabas','Onesimus'],
+   corrrectAnswer:3
+},
+{
+  question:" who was the son  Abraham had by his wife Sarah ?",
+   answers:['Ishmael','jacob',' isaac','Esau'],
+   corrrectAnswer:2
+},
+{
+  question:" Where did the LORD speak with Moses after they came out of Egypt ?",
+   answers:['Mount Horeh','Wilderness of Sinai',' Mount Olives','Camp of Reuben'],
+   corrrectAnswer:1
+},
+{
+  question:" Jesus said the Kingdom of God is like what seed ?",
+   answers:['Mustard','Pomegranate',' Grape','Fig'],
+   corrrectAnswer:0
+},
+{
+  question:" Who was mary and marthas brother ?",
+   answers:['James','Simon',' Aquila','Lazarus'],
+   corrrectAnswer:3
+},
+{
+  question:" what was with God and Manifested into us ?",
+   answers:['Firmament','Knowledge','Eternal life',' Discernment'],
+   corrrectAnswer:2
+},
+{
+  question:"Who is the Author of the Book of Songs of Song ?",
+   answers:['Solomon','Paul',' David','Manna'],
+   corrrectAnswer:0
+},
+{
+  question:" Whom did Jesus say should bury the Dead ?",
+   answers:['Family','The dead',' Friends','Priests'],
+   corrrectAnswer:1
+},
+{
+  question:" who had a dream of a ladder that reached to heaven ?",
+   answers:['Jacob','Abraham',' Daniel','Isaiah'],
+   corrrectAnswer:0
+},
+{
+  question:" Everyone that is proud in heart is what to the LORD ?",
+   answers:['A delight','A servant',' an aborminatio','A son'],
+   corrrectAnswer:2
+},
+{
+  question:" in what city did philip perform miracle ?",
+   answers:['Jerusalem','Sidon',' Samaria','Egypt'],
+   corrrectAnswer:2
+},
+{
+  question:" what was the land that GOD gave to Israelites ?",
+   answers:['Edom','Moab',' Midian','Canaan'],
+   corrrectAnswer:3
+},
+{
+  question:" what was simon peter's occupation before he met Jesus  ?",
+   answers:['Zealot','Tax collector',' tent maker','Fisher man'],
+   corrrectAnswer:3
+},
+{
+  question:"what is the name of the well that Abraham and abimelech quarrel over ?",
+   answers:['Param','Kedesh',' Shur','Beersheba'],
+   corrrectAnswer:3
+},
+{
+  question:"which angel appeared most often in daniels vision ?",
+   answers:['Thummim','Urim',' Gabriel','Michael'],
+   corrrectAnswer:2
+},
+{
+  question:"paul says the end of the law of righteousness is what ?",
+   answers:['God','Christ',' The Bible','Faith'],
+   corrrectAnswer:1
+},
+{
+  question:"How many thieves were crucified alongside of jesus ?",
+   answers:['3','1',' 2','4'],
+   corrrectAnswer:2
+},
+{
+  question:" In Isaiah chapter 6, Who did Isaiah see ?",
+   answers:['King of Israel','God',' his Father','His son'],
+   corrrectAnswer:1
+},
+{
+  question:" But those things which proceed out of the _____come forth from the _____ ?",
+   answers:['Heart,mouth','Mouth,mind','Mouth,Heart','mind,mouth'],
+   corrrectAnswer:2
+},
+{
+  question:" who were the recipients of the epistle to the Roman ?",
+   answers:['Caesar"s household','soilders who crucified jesus',' all the saints in rome','Catholic church'],
+   corrrectAnswer:2
+},
+{
+  question:"who warned paul of the plot against him ?",
+   answers:['His sister','His nephew',' His brother','Festus'],
+   corrrectAnswer:1
+},
+{
+  question:" How many days did God's creation take place ?",
+   answers:['5','6',' 8','7'],
+   corrrectAnswer:1
+},
+{
+  question:" In the new land the Israelites where no to do what with their formal enemies ?",
+   answers:['Worship','dine with',' forgive','Marry'],
+   corrrectAnswer:3
+},
+{
+  question:" What kind of person , according to the preacher of Ecclesiastes, is no better than an unenchanted Serpent ?",
+   answers:['A dreamer','a teller of secrets',' A coy woman','None of the above'],
+   corrrectAnswer:3
+},
+{
+  question:" Of what were the candlestickes of the Temple made ?",
+   answers:['silver','brass','wood','Gold'],
+   corrrectAnswer:3
+},
+{
+  question:" How was paul sent to italy ?",
+   answers:['By horseback','by sea',' By carriage','by foot'],
+   corrrectAnswer:1
+},
+{
+  question:" what must GOD servant not do ?",
+   answers:['drink wine','quarrel',' Eat meat','go to parties'],
+   corrrectAnswer:1
+},
+{
+  question:"what does paul call timothy in his opening salutation of his first letter to him ?",
+   answers:['son in the faith','brother in christ','lover of GOD','My peaceful child'],
+   corrrectAnswer:0
+},
+                                          
+]
+//initialize the game
+let currentQuestion=0;
+let score=0;
+
+//display the first question
+displayQuestion()
+//function to display the current question
+
+function displayQuestion(){
+const questionDisplay=
+document.getElementById("question-display");
+questionDisplay.innerHTML=
+questions[currentQuestion].question;
+
+const answerOptions=
+document.getElementById("answer-options");
+answerOptions.innerHTML="";
+for(let i=0;i<questions[currentQuestion].answers.length;i++){
+const answerOption=
+document.createElement("button");
+answerOption.innerHTML=
+questions[currentQuestion].answers[i];
+answerOption.onclick=checkAnswer;
+answerOptions.appendChild(answerOption);                         
+}
+}
+//function to check the players answer
+function checkAnswer(event){
+const playerAnswer=event.target.innerHTML;
+if(playerAnswer===questions[currentQuestion].answers[questions[currentQuestion].corrrectAnswer]){
+ score ++;
+ alert("correct👍");
+}
+else{
+alert("incorrect.The correct answer is  "   +    questions[currentQuestion].answers[questions[currentQuestion].corrrectAnswer]);                          
+}   
+currentQuestion++;
+if(currentQuestion>=questions.length){
+ alert("Game over! Your final score is "  +        score);                         
+} 
+else{
+ displayQuestion();                         
+}                      
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
